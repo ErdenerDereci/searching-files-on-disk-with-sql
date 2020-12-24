df3a83ef780d60dc4545627ca39d4d396f915c89
ErdenerDereci <43935534+ErdenerDereci@users.noreply.github.com>
2020-12-24 21:15:16 +0300

    s

--- sqlString/sqlString/Program.cs
+++ sqlString/sqlString/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -11,47 +12,219 @@ namespace sqlString
     {
         static void Main(string[] args)
         {
-            string sqlCumlesi = Console.ReadLine();
+            DirectoryInfo info =new DirectoryInfo(@"C:\Users\x\Desktop\didem");
 
-            if (kelimeKontrol(sqlCumlesi) == "NP")
+            long sda = 0;
+            
+            foreach(FileInfo x in info.GetFiles("*", SearchOption.AllDirectories))
             {
-                Console.WriteLine("Problem yok.");
+                sda += x.Length;
             }
-            else if(kelimeKontrol(sqlCumlesi) == "ilk kelime hatali")
+            Console.WriteLine(sda);
+            //while (true)
+            //{
+            //    string sqlCumlesi = Console.ReadLine();
+            //    Console.WriteLine("");
+            //    sqlSyntaxCheck(sqlCumlesi);
+            //    Console.WriteLine("---\n");
+            //}
+
+            bool xa = true;
+            //try
+            //{
+            //    string[] x = Directory.GetDirectories("C:");
+            //}
+            //catch
+            //{
+            //    xa = false;
+
+            //}
+
+            //if (xa)
+            //{
+            //    Console.WriteLine("Path is correct");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid path");
+            //}
+
+            
+            Console.ReadKey();
+        }
+        static string soldanBoslukSil(string sqlCumlesi,string noktalaIsareti)
+        {
+            while (sqlCumlesi != sqlCumlesi.Replace(" "+noktalaIsareti, noktalaIsareti))
             {
-                Console.WriteLine("Ilk kelime select,insert veya delete olmali");
+                sqlCumlesi = sqlCumlesi.Replace(" " + noktalaIsareti, noktalaIsareti);
+            }
+
+            return sqlCumlesi;
+        }
+        static string sagdanBoslukSil(string sqlCumlesi, string noktalaIsareti)
+        {
+            while (sqlCumlesi != sqlCumlesi.Replace( noktalaIsareti+ " ", noktalaIsareti))
+            {
+                sqlCumlesi = sqlCumlesi.Replace( noktalaIsareti + " ", noktalaIsareti);
+            }
+
+            return sqlCumlesi;
+        }
+        static string bosluklariTekBoslukYap(string sqlCumlesi)
+        {
+            while (sqlCumlesi != sqlCumlesi.Replace("  ", " "))
+            {
+                sqlCumlesi = sqlCumlesi.Replace("  ", " ");
+            }
+
+            return sqlCumlesi;
+        }
+        static string[] sqlCumlesiniDuzenle(string sqlCumlesi)
+        {
+            sqlCumlesi = sagdanBoslukSil(sqlCumlesi, ",");
+            sqlCumlesi = soldanBoslukSil(sqlCumlesi, ",");
+            sqlCumlesi = sagdanBoslukSil(sqlCumlesi, "=");
+            sqlCumlesi = soldanBoslukSil(sqlCumlesi, "=");
+            sqlCumlesi = sagdanBoslukSil(sqlCumlesi, "(");
+            sqlCumlesi = soldanBoslukSil(sqlCumlesi, ")");
+            sqlCumlesi = sagdanBoslukSil(sqlCumlesi, "*");
+            sqlCumlesi = soldanBoslukSil(sqlCumlesi, "*");
+            sqlCumlesi = sqlCumlesi.Replace("=", " = ");
+            sqlCumlesi = sqlCumlesi.Replace("*", " * ");
+            sqlCumlesi = bosluklariTekBoslukYap(sqlCumlesi);
+            sqlCumlesi = sqlCumlesi.Trim();
+            Console.WriteLine(sqlCumlesi);
+            string[] sqlYapisi = sqlCumlesi.Split(' ');
+            if (sqlYapisi[2]!="from")
+            {
+                sqlCumlesi=sqlCumlesi.Replace("* ","*");
+            }
+            Console.WriteLine(sqlCumlesi);
+            sqlYapisi = sqlCumlesi.Split(' ');
+            foreach(string c in sqlYapisi)
+            {
+                Console.WriteLine(c);
+            }
+            return sqlYapisi;
+
+        }
+        static void syntaxHatali()
+        {
+            Console.WriteLine("Syntax hatali");
+        }
+        static void sqlSyntaxCheck(string sqlCumlesi)
+        {
+            string[] sqlYapisi = sqlCumlesiniDuzenle(sqlCumlesi);
+            if (sqlYapisi[0].ToLower() =="select")
+            {
+                Console.WriteLine(selectSyntaxCheck(sqlYapisi));
             }
             else
             {
-                Console.WriteLine("ingilizce soz dizimi kullaniniz");
-            }
-           
-            Console.ReadKey();
+                Console.WriteLine("SQL Syntax Error: Ilk kelime select,insert veya delete olmali");
+            }                
         }
-        static string kelimeKontrol(string cumle)
+        static bool tabloStunElemanKontrol(string cumle)
         {
-            string[] kelimeler = cumle.Split(' ');
-
-            if (Regex.IsMatch(kelimeler[0], "^[a-zA-Z0-9]*$"))
+            
+            string[] parcalanmisCumle = cumle.Split(',');
+            string[] tabloStunlariDizi = { "*","name", "size", "permission", "hardlink", "user", "group", "modified date", "file type" };
+            List<string> tabloStunlari = new List<string>(tabloStunlariDizi);
+            foreach(string deger in parcalanmisCumle)
             {
-                if (kelimeler[0].ToLower()=="select"|| kelimeler[0].ToLower() == "insert"|| kelimeler[0].ToLower() == "delete")
+                if (tabloStunlari.Contains(deger))
                 {
-                    return "NP";
+                    tabloStunlari.Remove(deger);
                 }
                 else
                 {
-                    return "ilk kelime hatali";
+                    return false;
                 }
             }
-            else {
-                return "ingilizce soz dizimi kullaniniz";
-            }
-            
-
+            return true;
         }
-       
-        
-    }
+        static string selectSyntaxCheck(string[] sqlYapisi)
+        {
+            if (sqlYapisi.Length < 4)
+            {
+                return "SQL Syntax Error : Tam bir sql cumlesi girilmedi";
+            }
+            for(int i=1; i<sqlYapisi.Length; i++)
+            {
+                if (i == 1)
+                {
+                    if (!tabloStunElemanKontrol(sqlYapisi[i]))
+                    {
+                        return "SQL Syntax Error : Ikinci kelimede stun adi bulunamadi";
+                    }
+                    else
+                    {
+                        // Asil kodlar buraya
+                    }
+                }else if (i == 2)
+                {
+                    if (sqlYapisi[i] != "from")
+                    {
+                        return "SQL Syntax Error : Ucuncu kelime from olmali";
+                    }
+                }else if (i == 3)
+                {
+                    bool sart = true;
+                    // Path var mi kontrol ediliyor
+                    try
+                    {
+                        string[] x = Directory.GetDirectories(sqlYapisi[i]);
+                    }
+                    catch
+                    {
+                        sart = false;
+
+                    }
+
+                    if (sart)
+                    {
+                        
+                    }
+                    else
+                    {
+                        return "SQL Syntax Error : Path bulunamadi.";
+                    }
+                }else if (i == 4)
+                {
+                    if (sqlYapisi[i].ToLower() != "where")
+                    {
+                        return "SQL Syntax Error : Sart kelimesi hatali";
+                    }
+                    else
+                    {
+                        //Sart oldugu programa soylenecek
+                    }
+                }else if (i==5)
+                {
+                    
+                    if (!tabloStunElemanKontrol(sqlYapisi[i]))
+                    {
+                        return "SQL Syntax Error : Stun adi hatali";
+                    }
+                    else
+                    {
+                        //stun adi dogru. atama yapilacak 
+                    }
+                    
+                    // hic bir return e girilmediyse gerekli sartlar programa bildirilecek
+                }else if (i == 6)
+                {
+                    if (sqlYapisi[i] != "=")
+                    {
+                        return "SQL Syntax Error : '=' operatoru yazilmamis veya yanlis operator kullanilmis";
+                    }
+                }
+               
+            }
+            return "Syntax Dogru";
+        }
+    }   
+}
     
 
-}
+
