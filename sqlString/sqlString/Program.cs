25d845436320293591fa18e5f26c7cb05f8f119b
ErdenerDereci <43935534+ErdenerDereci@users.noreply.github.com>
2020-12-27 18:10:00 +0300

    ozlis

--- sqlString/sqlString/Program.cs
+++ sqlString/sqlString/Program.cs
@@ -12,24 +12,22 @@ namespace sqlString
     {
         static void Main(string[] args)
         {
-            DirectoryInfo info =new DirectoryInfo(@"C:\Users\x\Desktop\didem");
 
-            long sda = 0;
-            
-            foreach(FileInfo x in info.GetFiles("*", SearchOption.AllDirectories))
+            while (true)
             {
-                sda += x.Length;
+                string sqlCumlesi = Console.ReadLine();
+                Console.WriteLine("");
+                sqlSyntaxCheck(sqlCumlesi);
+                Console.WriteLine("---\n");
             }
-            Console.WriteLine(sda);
-            //while (true)
-            //{
-            //    string sqlCumlesi = Console.ReadLine();
-            //    Console.WriteLine("");
-            //    sqlSyntaxCheck(sqlCumlesi);
-            //    Console.WriteLine("---\n");
-            //}
+            //Console.WriteLine(sartCheck("permission=\"R\"&&sie=100k&&name=\"2\"size=asd"));
 
-            bool xa = true;
+            //string[] asd = { "&&" };
+            //List<string> sartListesi = new List<string>();
+            //sartListesi = "2&&".Split(asd, System.StringSplitOptions.None).ToList<string>();
+
+            //    Console.WriteLine(sartListesi.Count);
+
             //try
             //{
             //    string[] x = Directory.GetDirectories("C:");
@@ -49,7 +47,7 @@ namespace sqlString
             //    Console.WriteLine("Invalid path");
             //}
 
-            
+
             Console.ReadKey();
         }
         static string soldanBoslukSil(string sqlCumlesi,string noktalaIsareti)
@@ -89,13 +87,14 @@ namespace sqlString
             sqlCumlesi = soldanBoslukSil(sqlCumlesi, ")");
             sqlCumlesi = sagdanBoslukSil(sqlCumlesi, "*");
             sqlCumlesi = soldanBoslukSil(sqlCumlesi, "*");
-            sqlCumlesi = sqlCumlesi.Replace("=", " = ");
+            sqlCumlesi = sagdanBoslukSil(sqlCumlesi, "&");
+            sqlCumlesi = soldanBoslukSil(sqlCumlesi, "&");
             sqlCumlesi = sqlCumlesi.Replace("*", " * ");
             sqlCumlesi = bosluklariTekBoslukYap(sqlCumlesi);
             sqlCumlesi = sqlCumlesi.Trim();
             Console.WriteLine(sqlCumlesi);
             string[] sqlYapisi = sqlCumlesi.Split(' ');
-            if (sqlYapisi[2]!="from")
+            if (sqlYapisi.Length>3 && sqlYapisi[2]!="from")
             {
                 sqlCumlesi=sqlCumlesi.Replace("* ","*");
             }
@@ -108,9 +107,15 @@ namespace sqlString
             return sqlYapisi;
 
         }
-        static void syntaxHatali()
+        public static int areCornerQuote(string s)
         {
-            Console.WriteLine("Syntax hatali");
+            int n = s.Length;
+            if (n < 2)
+                return -1;
+            if (s[0] == s[n - 1] && s[0]=='"')
+                return 1;
+            else
+                return 0;
         }
         static void sqlSyntaxCheck(string sqlCumlesi)
         {
@@ -124,10 +129,185 @@ namespace sqlString
                 Console.WriteLine("SQL Syntax Error: Ilk kelime select,insert veya delete olmali");
             }                
         }
+        static string sartCheck(string sart ,string ikinciKelime)
+        {
+
+            List<string> ikinciKelimeSartiKontrolListesi = ikinciKelime.Split(',').ToList<string>();
+            string[] asd = { "&&" };
+            List<string> sartListesi = new List<string>();
+            sartListesi = sart.Split(asd,System.StringSplitOptions.None).ToList<string>();
+            
+            List<string> temp = new List<string>();
+            Dictionary<string, string> stunlarVeDegerler = new Dictionary<string, string>();
+            for(int i =0; i<sartListesi.Count; i++)
+            {
+                temp.Clear();
+
+                temp = sartListesi[i].Split('=').ToList<string>();
+                if (temp.Count < 2)
+                {
+                    return "SQL Syntax Error : \"=\" operatorunu kullaniniz.";
+                }
+                if (temp[0] == "")
+                {
+                    return "SQL Syntax Error : \"&&\" operatorunden sonra sart girilmedi";
+                }
+                if (temp[1] == "")
+                {
+                    return "SQL Syntax Error : Istenilen stuna yapilacak atamanin degeri girilmedi";
+                }
+                
+                if (temp.Count > 2)
+                {
+                    return "SQL Syntax Error : Sartlar && operatoru ile ayrilmali";
+                }
+                if (!tabloStunElemanKontrol(temp[0]))
+                {
+                    return "SQL Syntax Error : Stun adi dogru degil";
+                }
+                else
+                {
+                    try
+                    {
+                        ikinciKelimeSartiKontrolListesi.Remove(temp[0]);
+                    }
+                    catch
+                    {
+                        return "SQL Syntax Error : "+temp[0]+" stunu belirtilen stunlar arasinda yok. Atama yapilamaz. ";
+                    }
+                    if (temp[0] == "size")
+                    {
+                        
+                        if (temp[1].Last<char>() == 'm' || temp[1].Last<char>() == 'k' || temp[1].Last<char>() == 'g')
+                        {
+                            string size = temp[1].Substring(0, temp[1].Length - 1);
+                            if (!size.All(char.IsDigit))
+                            {
+                                return "SQL Syntax Error : Size degeri numerik olmali";
+                            }
+                            else
+                            {
+                                if (temp[1].Last<char>() == 'm')
+                                {
+                                    try
+                                    {
+                                        stunlarVeDegerler.Add(temp[0], (Convert.ToInt32(size) * 1024 * 1024).ToString());
+                                    }
+                                    catch
+                                    {
+                                        return "SQL Syntax Error : "+temp[0]+" stunu icin zaten atama yapilmis.";
+                                    }
+                                    
+                                }
+                                else if (temp[1].Last<char>() == 'k')
+                                {
+                                    try
+                                    {
+                                        stunlarVeDegerler.Add(temp[0], (Convert.ToInt32(size) * 1024).ToString());
+                                    }
+                                    catch
+                                    {
+                                        return "SQL Syntax Error : " + temp[0] + " stunu icin zaten atama yapilmis.";
+                                    }
+                                    
+                                }
+                                else
+                                {
+                                    
+                                    try
+                                    {
+                                        stunlarVeDegerler.Add(temp[0], (Convert.ToInt32(size) * 1024 * 1024 * 1024).ToString());
+                                    }
+                                    catch
+                                    {
+                                        return "SQL Syntax Error : " + temp[0] + " stunu icin zaten atama yapilmis.";
+                                    }
+                                    
+                                }
+                            }
+                        }
+                        else
+                        {
+                            return "SQL Syntax Error : Size degeri belirtilmedi.(k,g,m)";
+                        }
+                    }
+                    else if (temp[0] == "permission")
+                    {
+                        if (areCornerQuote(temp[1])!=1)
+                        {
+                            return "SQL Syntax Error : \" isareti kullanilmadi.";
+                        }
+                        else if (temp[1] == "\"R\"" || temp[1] == "\"W\"" || temp[1] == "\"X\"")
+                        {
+                            try
+                            {
+                                stunlarVeDegerler.Add(temp[0], temp[1]);
+                            }
+                            catch
+                            {
+                                return "SQL Syntax Error : " + temp[0] + " stunu icin zaten atama yapilmis.";
+                            }
+                        }
+                        else
+                        {
+                            return "SQL Syntax Error : Yanlis permisson degeri girildi.(R,W,X)";
+                        }
+                    }
+                    else if (temp[0] == "name")
+                    {
+                        if (areCornerQuote(temp[1]) != 1)
+                        {
+                            return "SQL Syntax Error : \" isareti kullanilmadi. String deger giriniz.";
+                        }
+                        else
+                        {
+                            try
+                            {
+                                stunlarVeDegerler.Add(temp[0], temp[1]);
+                            }
+                            catch
+                            {
+                                return "SQL Syntax Error : " + temp[0] + " stunu icin zaten atama yapilmis.";
+                            }
+                            
+                        }
+                    }
+                }
+            }
+            if (ikinciKelimeSartiKontrolListesi.Count != 0)
+            {
+                string tempString = "";
+                foreach (string a in ikinciKelimeSartiKontrolListesi)
+                {
+                    tempString += a + " ";
+                }
+                return "SQL Syntax Error : " + tempString + "stunlarina atama yapilmamis.";
+            }
+            //foreach (string x in sartListesi)
+            //{
+            //    Console.WriteLine(x);
+            //}
+            //for(int i=0; i<stunlarVeDegerler.Count; i++)
+            //{
+            //    Console.WriteLine(stunlarVeDegerler["size"]);
+            //}
+            return "pass";
+        }
         static bool tabloStunElemanKontrol(string cumle)
+        { 
+            string[] tabloStunlariDizi = { "*", "name", "size", "permission", "hardlink", "user", "group", "modified date", "file type" };
+            List<string> tabloStunlari = new List<string>(tabloStunlariDizi);
+            
+                if (!tabloStunlari.Contains(cumle))
+                {
+                return false;
+                }
+            return true;
+        }
+        static bool tabloStunElemanKontrol(string cumle,char split)
         {
             
-            string[] parcalanmisCumle = cumle.Split(',');
+            string[] parcalanmisCumle = cumle.Split(split);
             string[] tabloStunlariDizi = { "*","name", "size", "permission", "hardlink", "user", "group", "modified date", "file type" };
             List<string> tabloStunlari = new List<string>(tabloStunlariDizi);
             foreach(string deger in parcalanmisCumle)
@@ -153,12 +333,20 @@ namespace sqlString
             {
                 if (i == 1)
                 {
-                    if (!tabloStunElemanKontrol(sqlYapisi[i]))
+                    if (!tabloStunElemanKontrol(sqlYapisi[i],','))
                     {
                         return "SQL Syntax Error : Ikinci kelimede stun adi bulunamadi";
                     }
                     else
                     {
+                        try
+                        {
+                            string x=sqlYapisi[5];
+                        }
+                        catch
+                        {
+                            return "SQL Syntax Error : Belirtilen sartlarin atamasi yapilmali";
+                        }
                         // Asil kodlar buraya
                     }
                 }else if (i == 2)
@@ -197,29 +385,32 @@ namespace sqlString
                     }
                     else
                     {
-                        //Sart oldugu programa soylenecek
+                        if (sqlYapisi.Length < 6)
+                        {
+                            return "SQL Syntax Error : Sart girilmedi";
+                        }
+                        else
+                        {
+                            // sart atamasi yapilacak
+                        }
+                        
                     }
-                }else if (i==5)
+                }else if (sqlYapisi.Length>6)
+                {
+                    return "SQL Syntax Error : Sartlar bosluk ile baglanmis. && operatoru kullaniniz.";
+                }
+                else if (i == 5)
                 {
                     
-                    if (!tabloStunElemanKontrol(sqlYapisi[i]))
+                    if (sartCheck(sqlYapisi[i], sqlYapisi[1]) == "pass")
                     {
-                        return "SQL Syntax Error : Stun adi hatali";
+
                     }
                     else
                     {
-                        //stun adi dogru. atama yapilacak 
-                    }
-                    
-                    // hic bir return e girilmediyse gerekli sartlar programa bildirilecek
-                }else if (i == 6)
-                {
-                    if (sqlYapisi[i] != "=")
-                    {
-                        return "SQL Syntax Error : '=' operatoru yazilmamis veya yanlis operator kullanilmis";
+                        return sartCheck(sqlYapisi[i], sqlYapisi[1]);
                     }
                 }
-               
             }
             return "Syntax Dogru";
         }
--- testWeb/testWeb/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="testWeb.MvcApplication" Language="C#" %>
